743b04ec1dd95721e7f8486e808c5e3bd0525f0b
senasEtec <143223228+senasEtec@users.noreply.github.com>
2023-08-25 18:12:41 -0300

    inicio do projeto react

---
+++ src/App.js
@@ -0,0 +1,37 @@
+function MyButton() {
+
+  return (
+  
+  <button>
+  
+  I'm a button
+  
+  </button>
+  
+  );
+  
+  }
+  
+   
+  
+  export default function MyApp() {
+  
+  return (
+  
+  <div>
+  
+  <h1>Gustavo Bruno Da Costa</h1>
+  
+  <p>Tenho 16 anos;</p>
+  
+  <p>Estudo na Etec Polivalente e no Senai em Americana (SP), cursnado Desenvolvimento de Sistemas;</p>
+  
+  <p>Nasci na cidade de Santa Barbara Do Oeste (SP); </p>
+  
+  <p>Gosto de esportes, músicas, livros, filmes, séries,carros, motos, dentre outras coisas;</p>
+
+  </div>
+  
+  );
+  
+  }
